48125789f00f0c2998a2d9151b031db39a6202fa
drblix <benjamin800823@gmail.com>
2023-08-13 18:43:57 -0400

    Initial commit

---
+++ NumberSystemConverter/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace NumberSystemConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fromComboBox = new System.Windows.Forms.ComboBox();
+            this.fromTextBox = new System.Windows.Forms.TextBox();
+            this.fromGroupBox = new System.Windows.Forms.GroupBox();
+            this.toGroupBox = new System.Windows.Forms.GroupBox();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.toComboBox = new System.Windows.Forms.ComboBox();
+            this.toTextBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.fromGroupBox.SuspendLayout();
+            this.toGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fromComboBox
+            // 
+            this.fromComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.fromComboBox.FormattingEnabled = true;
+            this.fromComboBox.Items.AddRange(new object[] { "Decimal", "Binary", "Hexadecimal" });
+            this.fromComboBox.Location = new System.Drawing.Point(61, 21);
+            this.fromComboBox.Name = "fromComboBox";
+            this.fromComboBox.Size = new System.Drawing.Size(121, 24);
+            this.fromComboBox.TabIndex = 0;
+            this.fromComboBox.Text = "Number System";
+            this.fromComboBox.SelectedIndexChanged += new System.EventHandler(this.fromComboBox_SelectedIndexChanged);
+            // 
+            // fromTextBox
+            // 
+            this.fromTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fromTextBox.Location = new System.Drawing.Point(17, 51);
+            this.fromTextBox.Name = "fromTextBox";
+            this.fromTextBox.Size = new System.Drawing.Size(207, 22);
+            this.fromTextBox.TabIndex = 2;
+            // 
+            // fromGroupBox
+            // 
+            this.fromGroupBox.Controls.Add(this.fromComboBox);
+            this.fromGroupBox.Controls.Add(this.fromTextBox);
+            this.fromGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fromGroupBox.Location = new System.Drawing.Point(24, 12);
+            this.fromGroupBox.Name = "fromGroupBox";
+            this.fromGroupBox.Size = new System.Drawing.Size(239, 92);
+            this.fromGroupBox.TabIndex = 3;
+            this.fromGroupBox.TabStop = false;
+            this.fromGroupBox.Text = "From";
+            // 
+            // toGroupBox
+            // 
+            this.toGroupBox.Controls.Add(this.copyButton);
+            this.toGroupBox.Controls.Add(this.toComboBox);
+            this.toGroupBox.Controls.Add(this.toTextBox);
+            this.toGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toGroupBox.Location = new System.Drawing.Point(24, 139);
+            this.toGroupBox.Name = "toGroupBox";
+            this.toGroupBox.Size = new System.Drawing.Size(239, 111);
+            this.toGroupBox.TabIndex = 4;
+            this.toGroupBox.TabStop = false;
+            this.toGroupBox.Text = "To";
+            // 
+            // copyButton
+            // 
+            this.copyButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.copyButton.Location = new System.Drawing.Point(78, 79);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(75, 23);
+            this.copyButton.TabIndex = 3;
+            this.copyButton.Text = "Copy";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // toComboBox
+            // 
+            this.toComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.toComboBox.FormattingEnabled = true;
+            this.toComboBox.Items.AddRange(new object[] { "Decimal", "Binary", "Hexadecimal" });
+            this.toComboBox.Location = new System.Drawing.Point(61, 21);
+            this.toComboBox.Name = "toComboBox";
+            this.toComboBox.Size = new System.Drawing.Size(121, 24);
+            this.toComboBox.TabIndex = 0;
+            this.toComboBox.Text = "Number System";
+            this.toComboBox.SelectedIndexChanged += new System.EventHandler(this.toComboBox_SelectedIndexChanged);
+            // 
+            // toTextBox
+            // 
+            this.toTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.toTextBox.Location = new System.Drawing.Point(17, 51);
+            this.toTextBox.Name = "toTextBox";
+            this.toTextBox.ReadOnly = true;
+            this.toTextBox.Size = new System.Drawing.Size(207, 22);
+            this.toTextBox.TabIndex = 2;
+            // 
+            // convertButton
+            // 
+            this.convertButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.convertButton.Location = new System.Drawing.Point(94, 113);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(92, 23);
+            this.convertButton.TabIndex = 5;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 258);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.toGroupBox);
+            this.Controls.Add(this.fromGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Number System Converter";
+            this.fromGroupBox.ResumeLayout(false);
+            this.fromGroupBox.PerformLayout();
+            this.toGroupBox.ResumeLayout(false);
+            this.toGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button convertButton;
+
+        private System.Windows.Forms.Button copyButton;
+
+        private System.Windows.Forms.GroupBox toGroupBox;
+        private System.Windows.Forms.ComboBox toComboBox;
+        private System.Windows.Forms.TextBox toTextBox;
+
+        private System.Windows.Forms.GroupBox fromGroupBox;
+
+        private System.Windows.Forms.TextBox fromTextBox;
+
+        private System.Windows.Forms.ComboBox fromComboBox;
+
+        #endregion
+    }
+}
---
+++ NumberSystemConverter/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NumberSystemConverter
+{
+    public partial class Form1 : Form
+    {
+        private enum NumberSystem
+        {
+            Decimal,
+            Binary,
+            Hexadecimal
+        }
+
+        private static readonly Dictionary<ulong, char> DecimalToHexaDict = new Dictionary<ulong, char>()
+        {
+            { 0, '0' },
+            { 1, '1' },
+            { 2, '2' },
+            { 3, '3' },
+            { 4, '4' },
+            { 5, '5' },
+            { 6, '6' },
+            { 7, '7' },
+            { 8, '8' },
+            { 9, '9' },
+            { 10, 'A' },
+            { 11, 'B' },
+            { 12, 'C' },
+            { 13, 'D' },
+            { 14, 'E' },
+            { 15, 'F' }
+        };
+
+        private static readonly Dictionary<char, ulong> HexaToDecimalDict = new Dictionary<char, ulong>()
+        {
+            { '0', 0 },
+            { '1', 1 },
+            { '2', 2 },
+            { '3', 3 },
+            { '4', 4 },
+            { '5', 5 },
+            { '6', 6 },
+            { '7', 7 },
+            { '8', 8 },
+            { '9', 9 },
+            { 'A', 10 },
+            { 'B', 11 },
+            { 'C', 12 },
+            { 'D', 13 },
+            { 'E', 14 },
+            { 'F', 15 },
+        };
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object _, EventArgs __)
+        {
+            // Did the user enter anything?
+            if (fromTextBox.Text.Trim() == string.Empty)
+            {
+                ShowErrorBox("No number to convert");
+                return;
+            }
+            
+            // Removing all spaces in the user's input
+            string textToConvert = fromTextBox.Text.Trim().Replace(" ", string.Empty);
+            
+            // Are both number system dropdowns valid?
+            if (fromComboBox.SelectedIndex == -1 || toComboBox.SelectedIndex == -1)
+            {
+                ShowErrorBox("Pick a number system");
+                return;
+            }
+
+            NumberSystem fromSystem = (NumberSystem)fromComboBox.SelectedIndex;
+            NumberSystem toSystem = (NumberSystem)toComboBox.SelectedIndex;
+            
+            if (fromSystem == NumberSystem.Decimal)
+            {
+                switch (toSystem)
+                {
+                    case NumberSystem.Binary when ulong.TryParse(textToConvert, out ulong parsedText):
+                        toTextBox.Text = DecimalToBinary(parsedText);
+                        break;
+                    case NumberSystem.Binary:
+                        ShowErrorBox("Failed to parse decimal number to binary. Check if number is negative");
+                        break;
+                    case NumberSystem.Hexadecimal when ulong.TryParse(textToConvert, out ulong parsedText):
+                        toTextBox.Text = DecimalToHexadecimal(parsedText);
+                        break;
+                    case NumberSystem.Hexadecimal:
+                        ShowErrorBox("Failed to parse decimal number to hexadecimal. Check if number is negative");
+                        break;
+                    default:
+                        ShowErrorBox("Invalid number system to convert to");
+                        break;
+                }
+            }
+            else if (fromSystem == NumberSystem.Binary)
+            {
+                if (toSystem == NumberSystem.Decimal)
+                    toTextBox.Text = BinaryToDecimal(fromTextBox.Text).ToString();
+                else if (toSystem == NumberSystem.Hexadecimal)
+                    toTextBox.Text = DecimalToHexadecimal( ulong.Parse(BinaryToDecimal(fromTextBox.Text).ToString()));
+            }
+            else
+            {
+                if (toSystem == NumberSystem.Decimal)
+                    toTextBox.Text = HexadecimalToDecimal(fromTextBox.Text).ToString();
+                else if (toSystem == NumberSystem.Binary)
+                    toTextBox.Text = DecimalToBinary(ulong.Parse(HexadecimalToDecimal(fromTextBox.Text).ToString()));
+            }
+        }
+        
+        private void fromComboBox_SelectedIndexChanged(object _, EventArgs __)
+        {
+            if (fromComboBox.SelectedIndex == toComboBox.SelectedIndex)
+            {
+                if (fromComboBox.SelectedIndex - 1 < 0)
+                    toComboBox.SelectedIndex = 2;
+                else
+                    toComboBox.SelectedIndex = fromComboBox.SelectedIndex - 1;
+            }
+        }
+
+        private void toComboBox_SelectedIndexChanged(object _, EventArgs __)
+        {
+            if (toComboBox.SelectedIndex == fromComboBox.SelectedIndex)
+            {
+                if (toComboBox.SelectedIndex - 1 < 0)
+                    fromComboBox.SelectedIndex = 2;
+                else
+                    fromComboBox.SelectedIndex = toComboBox.SelectedIndex - 1;
+            }
+        }
+
+        private static string DecimalToBinary(ulong number)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            // Main driver
+            while (number > 0)
+            {
+                stringBuilder.Append(number % 2);
+                number /= 2;
+            }
+            
+            // Appending zeroes until a factor of 4 is reached
+            // Creates groups of fours
+            while (stringBuilder.Length % 4 != 0)
+                stringBuilder.Append('0');
+            
+            // Reversing the built string
+            char[] charArray = stringBuilder.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private static string DecimalToHexadecimal(ulong number)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            // Main driver
+            // '?' is appended when unknown value is encountered
+            while (number > 0)
+            {
+                stringBuilder.Append(DecimalToHexaDict.TryGetValue(number % 16, out char hexChar) ? hexChar : '?');
+                number /= 16;
+            }
+            
+            // Reversing the built string
+            char[] charArray = stringBuilder.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+        
+        private static ulong BinaryToDecimal(string binary)
+        {
+            ulong num = 0;
+            int pow = 0;
+            for (int i = binary.Length - 1; i >= 0; i--)
+            {
+                if (binary[i] == '1')
+                    num += (ulong)Math.Pow(2, pow);
+
+                pow++;
+            }
+
+            return num;
+        }
+
+        private static ulong HexadecimalToDecimal(string hexa)
+        {
+            ulong num = 0;
+            int pow = 0;
+            for (int i = hexa.Length - 1; i >= 0; i--)
+            {
+                if (HexaToDecimalDict.TryGetValue(hexa[i], out ulong val))
+                    num += (ulong)Math.Pow(16, pow) * val;
+                else
+                    ShowErrorBox("Failed to convert hexadecimal to decimal");
+
+                pow++;
+            }
+
+            return num;
+        }
+
+        private void copyButton_Click(object _, EventArgs __) => Clipboard.SetText(toTextBox.Text);
+        private static void ShowErrorBox(string message) => MessageBox.Show(message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+}
